d3fcec7f8823b22d7a6cd0e202a4105ebcc413aa
Sergio Oseko <osk.sergio@gmail.com>
2021-12-11 22:53:55 -0300

    feat: start of class on conditionals

---
+++ 03_12_2016/aula.rb
@@ -0,0 +1,18 @@
+require 'byebug'
+
+a = 1
+
+
+if a ==1 
+    puts "O conteúdo da variável a é igual a: #{a}." 
+end
+
+if a ==1 then puts "O conteúdo da variável a é igual a: #{a}." end
+
+if a ==1 ; puts "O conteúdo da variável a é igual a: #{a}." ; end
+
+if (a==1)
+    puts "O conteúdo da variável a é igual a: #{a}."
+else
+    puts "O conteúdo da variável a não é igual a 1."    
+end
