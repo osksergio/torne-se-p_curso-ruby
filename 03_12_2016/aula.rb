cc3e7e02d694cdefea7eb1b9d61ac0dc6a13884c
Sergio Oseko <osk.sergio@gmail.com>
2021-12-12 09:20:24 -0300

    feat: more conditioonals

--- 03_12_2016/aula.rb
+++ 03_12_2016/aula.rb
@@ -16,3 +16,22 @@ if (a==1)
 else
     puts "O conteúdo da variável a não é igual a 1."    
 end
+
+a = 3
+if a == 1
+    puts "O valo de a é igual a 1."
+elsif a == 3
+    puts "O valor de a é igual a 3."
+else
+    puts "O valo de a é dirente de 1."
+end
+
+unless a == 1
+    puts "O valor de a é diferente de 1."
+end
+
+a = 4
+puts "O valor de a é: #{a}" if a == 4
+
+a = a == 3 ? 50 : 40
+puts "O valor final de a com o ternário é: #{a}."
