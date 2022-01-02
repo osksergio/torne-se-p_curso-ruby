93a655dbf82dc9a79e25a57b7625dc19cd495eb1
Sergio Oseko <osk.sergio@gmail.com>
2022-01-01 21:41:17 -0300

    feat: more strings handling (capitalize, delete, split, reverse, etc

--- 09_12_2016/aula.rb
+++ 09_12_2016/aula.rb
@@ -19,5 +19,45 @@ a = "nossa aula de hoje " + b
 puts a
 
 # exibir uma string em forma de lista separada qdo ocorrer o caracter informado
-# exemplo abaixo a string será dividida em uma lista separada por um espaço
+# exemplo abaixo a string será dividida em uma lista separada pelo espaço em branco
 puts a.split(' ')
+
+# retornar o trecho de uma string informando o início e a quantidade de caracteres
+# no exemplo abaixo será retornado "nossa"
+puts a[0, 5]
+
+# o método scan também pode ser utilizado para retornar um trecho da string
+# neste caso é passado como parâmetro uma expressão regular
+puts a.scan(/aula d/)
+
+# upcase deixas as letras maiúsculas
+puts a.upcase
+
+# downcase deixa as letras minúsculas
+puts a.downcase
+
+# capitalize retorna a string com a primeira letra em maiúscula.
+puts a.capitalize
+
+# para excluir um trecho da string... usar o delete
+s = "NOSSA aula de hoje"
+puts s.delete("de hoje")
+
+# strip  --> remove os espaços em branco
+# lstrip --> remove os espaços em branco a esquerda
+# rstrip --> remove os espaços em branco a direita
+string_com_espacos = "       Nossa aula de hoje.         "
+puts string_com_espacos.strip
+puts string_com_espacos.lstrip
+puts string_com_espacos.rstrip
+
+# include? --> retorna true ou false qdo ocorrer a string informada
+puts string_com_espacos.include?("aula")
+puts string_com_espacos.include?("de hoje")
+puts string_com_espacos.include?("abobrinha")
+
+# index --> retorna o índice de algum trecho informado existente na string
+puts string_com_espacos.index("Nossa")
+
+# reverse --> inverte toda a string
+puts string_com_espacos.reverse
