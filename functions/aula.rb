46abe74eceeda3a27e5376fdf1318a17d6cb31e2
Sergio Oseko <osk.sergio@gmail.com>
2022-01-10 23:15:34 -0300

    feat: examples of functions

--- functions/aula.rb
+++ functions/aula.rb
@@ -2,36 +2,33 @@
 
 require 'byebug'
 
-# exemplo de um hash
-# a = { nome: 'sergio', tel: '981016963' }
-# puts a
-# puts a[:nome]
-# puts a[:tel]
-
-=begin
-loop do
-  puts 'Bem-vindo ao programa'
-  puts 'Digite 0 para sair ou 1 para continuar'
-  valor = gets.to_i
-
-  break if valor == 0
-
-  alunos = []
-  3.times do
-    aluno = {}
-
-    puts 'Digite o nome do aluno'
-    aluno[:nome] = gets.delete("\n")
-
-    puts 'Digite o telefone do aluno'
-    aluno[:telefone] = gets.delete("\n")
-
-    alunos << aluno
-  end
-
-  alunos.each do |aluno|
-    puts '========================================================='
-    puts "Aluno: #{aluno[:nome]}, Telefone: #{aluno[:telefone]}"
-  end
+# funcao com parametro de valor default
+def calculo(count = 1)
+  return 1 if count == 2
+  1 + 420 - 3
 end
-=end
+
+a = calculo(2)
+puts a
+
+# função com quantidade dinâmica de parâmetros
+def calculo2(*args)
+  return 1 if args.include? 2 # se ocorrer o número 2 nos parâmetros retorna 1
+  1 + 420 - 3
+end
+
+b = calculo2 1,2,3,4,5,6
+puts b
+
+# exemplo de função com tratamento de erro através do "rescue"
+def calculo3(*args)
+  return 1 if args.include? 2 # se ocorrer o número 2 nos parâmetros retorna 1
+  a = "aa" + 1
+  1 + 420 - 3
+rescue Exception => e
+  puts "Ocorreu um erro!"
+  puts e.message
+end
+
+c = calculo3 1,3,4,5,6
+puts c
