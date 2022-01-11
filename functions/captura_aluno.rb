fbcc030ca1f4ba7587cb48138fe2ecaa8293e133
Sergio Oseko <osk.sergio@gmail.com>
2022-01-10 23:16:30 -0300

    feat: function captura_aluno

---
+++ functions/captura_aluno.rb
@@ -0,0 +1,32 @@
+require 'byebug'
+
+def captura_aluno
+  aluno = {}
+  puts 'Digite o nome do aluno'
+  aluno[:nome] = gets.delete("\n")
+  puts 'Digite o telefone do aluno'
+  aluno[:telefone] = gets.delete("\n")
+  aluno
+end
+
+def mostrar_alunos(alunos)
+  alunos.each do |aluno|
+    puts '========================================================='
+    puts "Aluno: #{aluno[:nome]}, Telefone: #{aluno[:telefone]}"
+  end
+end
+
+loop do
+  puts 'Bem-vindo ao programa'
+  puts 'Digite 0 para sair ou 1 para continuar'
+  valor = gets.to_i
+
+  break if valor == 0
+
+  alunos = []
+  3.times do
+    alunos << captura_aluno
+  end
+
+  mostrar_alunos(alunos)
+end
